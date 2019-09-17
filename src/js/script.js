5249364a07c7ef8ad5823959fbc4242a56dde63a
d3rry <taranow.daniil@yandex.ru>
2019-09-17 20:32:40 +0300

    added jquery validation

--- src/js/script.js
+++ src/js/script.js
@@ -49,4 +49,35 @@ $(document).ready(function(){
             $('.overlay, #order').fadeIn();
         })
     })
+
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите своё имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                  required: "Пожалуйста, введите свою почту",
+                  email: "Неправильно введён адрес почты"
+                }
+              }
+        });
+    };
+
+    validateForms('#consultation form');
+    validateForms('#consultation-form');
+    validateForms('#order form');
 });
